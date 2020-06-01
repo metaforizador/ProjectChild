ae4217c25d93b913e19e8796daf1e7eb3d351158
Toni <toni.heinonen@hotmail.fi>
2020-06-01 13:42:29 +0300

    Add scriptable object Answer

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Answer", menuName ="Dialogue / New Answer")]
+public class AnswerData : Answer { }
+
+[System.Serializable]
+public class Answer : ScriptableObject {
+
+    public new string name;
+    public string text;
+
+}
