5e9ba8b10436145399d9b0c87c6b5137015a4c6b
Toni <toni.heinonen@hotmail.fi>
2020-06-01 12:11:07 +0300

    Add script for DialogueCanvas.

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMeshProUGUI question;
+    
+    void Start() {
+        question.text = "Hellurei";
+    }
+
+    void Update() {
+        
+    }
+}
