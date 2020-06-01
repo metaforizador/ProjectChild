9ebe4ea540554108db797501464d64c4bec81a49
Toni <toni.heinonen@hotmail.fi>
2020-06-01 12:40:13 +0300

    Add buttons to list dynamically.

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using TMPro;
 
 public class DialogueScript : MonoBehaviour
@@ -9,9 +6,21 @@ public class DialogueScript : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI question;
+
+    [SerializeField]
+    private GameObject answerListView, answerButtonPrefab;
     
     void Start() {
-        question.text = "Hellurei";
+        for (int i = 0; i < 5; i++) {
+            GameObject button = Instantiate(answerButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+            // Add button to the list and set the scale to 1 (parent.transform changes it to 0,6)
+            button.transform.parent = answerListView.transform;
+            button.transform.localScale = Vector3.one;
+
+            // Set text to answer button
+            button.GetComponentInChildren<TextMeshProUGUI>().text = "Answer: " + i;
+        }
     }
 
     void Update() {
