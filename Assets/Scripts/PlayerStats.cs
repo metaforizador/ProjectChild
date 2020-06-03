d4e82e11c5051b194494ee54f157a36037fef7c8
Toni <toni.heinonen@hotmail.fi>
2020-06-03 10:38:05 +0300

    Add singleton PlayerStats class

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    
+    // Make class static and destroy if script already exists
+    private static PlayerStats _instance; // **<- reference link to the class
+    public static PlayerStats Instance { get { return _instance; } }
+
+    private void Awake() {
+        if (_instance == null) {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
