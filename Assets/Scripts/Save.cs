674ec1994ae45cb66fcbaf327cb9b5a7584b91dc
Toni <toni.heinonen@hotmail.fi>
2020-06-05 09:30:10 +0300

    Add code to save and load player stats
    Saving and loading is not yet tested, so I'm not 100% sure it works.

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public class Save {
+    // Player stats
+    public int maxHp, maxShield, level, xp, shieldRegen, armor, resistance, attackSpd, fireRate, dodge, critical, movementSpd;
+
+    private Save CreateSaveGameObject() {
+        Save save = new Save();
+        PlayerStats.Instance.SavePlayerStats(save);
+
+        return save;
+    }
+
+    public void SaveGame() {
+        Save save = CreateSaveGameObject();
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        Debug.Log("Game Saved");
+    }
+
+    public void LoadGame() {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            PlayerStats.Instance.LoadPlayerStats(save);
+            file.Close();
+
+            Debug.Log("Game Loaded");
+        } else {
+            Debug.Log("No game saved!");
+        }
+    }
+}
--- Assets/Scripts/Singletons/PlayerStats.cs
+++ Assets/Scripts/Singletons/PlayerStats.cs
@@ -13,11 +13,65 @@ public class PlayerStats : MonoBehaviour
         if (_instance == null) {
             _instance = this;
             DontDestroyOnLoad(gameObject);
+            SetDefaultStats();
         } else {
             Destroy(gameObject);
         }
     }
 
+    // Stats to save and load (Declare defaults here)
     [SerializeField]
-    private int hp, shield, xp, shieldRegen, armor, resistance, attackSpd, fireRate, dodge, critical, movementSpd;
+    private int maxHp, maxShield, level, xp, shieldRegen, armor, resistance, attackSpd, fireRate, dodge, critical, movementSpd;
+
+    // Temporary stats
+    [SerializeField]
+    private int hp, shield;
+
+    private void SetDefaultStats() {
+        maxHp = 70;
+        maxShield = 70;
+        level = 1;
+        xp = 0;
+        shieldRegen = 12;
+        armor = 8;
+        resistance = 8;
+        attackSpd = 13;
+        fireRate = 13;
+        dodge = 20;
+        critical = 20;
+        movementSpd = 12;
+
+        hp = maxHp;
+        shield = maxShield;
+    }
+
+    public void SavePlayerStats(Save save) {
+        save.maxHp = maxHp;
+        save.maxShield = maxShield;
+        save.level = level;
+        save.xp = xp;
+        save.shieldRegen = shieldRegen;
+        save.armor = armor;
+        save.resistance = resistance;
+        save.attackSpd = attackSpd;
+        save.fireRate = fireRate;
+        save.dodge = dodge;
+        save.critical = critical;
+        save.movementSpd = movementSpd;
+    }
+
+    public void LoadPlayerStats(Save save) {
+        maxHp = save.maxHp;
+        maxShield = save.maxShield;
+        level = save.level;
+        xp = save.xp;
+        shieldRegen = save.shieldRegen;
+        armor = save.armor;
+        resistance = save.resistance;
+        attackSpd = save.attackSpd;
+        fireRate = save.fireRate;
+        dodge = save.dodge;
+        critical = save.critical;
+        movementSpd = save.movementSpd;
+    }
 }
