312dc7b21ddf5d964129e58838b24b2824b6cd64
Toni <toni.heinonen@hotmail.fi>
2020-06-09 15:50:51 +0300

    Loop through all the replies
    - Don't show same replies before all of the replies has been shown.

--- Assets/Scripts/Dialogue/XMLDialogueParser.cs
+++ Assets/Scripts/Dialogue/XMLDialogueParser.cs
@@ -69,7 +69,7 @@ public class XMLDialogueParser {
         List<Question> questions = new List<Question>();
 
         XMLDialogueParser data = Load();
-        // Add all questions to list which belong to this area
+        // Add all questions to list which belong to this mood
         foreach (Question o in data.questions) {
             if (o.mood.Equals(mood)) {
                 questions.Add(o);
@@ -84,7 +84,7 @@ public class XMLDialogueParser {
         // Retrieve already asked questions and loop through remaining ones
         Dictionary<Mood, List<string>> askedQuestions = CanvasMaster.Instance.askedQuestions;
 
-        // If askedQuestions doesn't have current area as key yet, create new key
+        // If askedQuestions doesn't have current mood as key yet, create new key
         if (!askedQuestions.ContainsKey(mood)) {
             askedQuestions.Add(mood, new List<string>());
         }
@@ -130,10 +130,35 @@ public class XMLDialogueParser {
             }
         }
 
-        // If count is 5, random returns values between 0 and 4
+        // Retrieve already given replies and loop through remaining ones
+        Dictionary<WordsType, List<string>> givenReplies = CanvasMaster.Instance.givenReplies;
 
-        Reply randomReply = replies[UnityEngine.Random.Range(0, replies.Count)];
+        // If givenReplies doesn't have current type as key yet, create new key
+        if (!givenReplies.ContainsKey(type)) {
+            givenReplies.Add(type, new List<string>());
+        }
 
-        return randomReply.replyText;
+        Reply reply;
+        while (true) {
+            // If count is 5, random returns values between 0 and 4
+            reply = replies[UnityEngine.Random.Range(0, replies.Count)];
+
+            // If reply has already been given, randomize new one
+            if (givenReplies[type].Contains(reply.replyText)) {
+                continue;
+            }
+
+            // Add reply to given replies
+            givenReplies[type].Add(reply.replyText);
+
+            // If all of the replies have been given from this type, clear list
+            if (givenReplies[type].Count == replies.Count) {
+                givenReplies[type].Clear();
+            }
+
+            break;
+        }
+
+        return reply.replyText;
     }
 }
