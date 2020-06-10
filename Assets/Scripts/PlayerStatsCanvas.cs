50852db1fa3f256815d9312e8f17efd50e54cba1
Toni <toni.heinonen@hotmail.fi>
2020-06-10 14:27:23 +0300

    Add references to stats numbers in script

---
+++ Assets/Scripts/PlayerStatsCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerStatsCanvas : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI shieldNum, staminaNum, ammoNum, dodgeNum, criticalNum, rareItemFindNum,
+        piercingDmgNum, kineticDmgNum, energyDmgNum, piercingResNum, kineticResNum, energyResNum,
+        attackNum, movementNum, fireRateNum, levelNum, xpNum, nextLevelNum;
+}
