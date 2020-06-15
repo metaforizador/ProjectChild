6e5ee9adb0957ce735df653a2efcecfaff344f92
Toni <toni.heinonen@hotmail.fi>
2020-06-15 12:55:47 +0300

    Create very basic enemy script for enemy stats
    - Create Enemy prefab.
    - Add Tag "Enemy".

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float hp = 100;
+    public bool alive = true;
+
+    public void TakeDamage(float amount) {
+        hp -= amount;
+
+        if (hp <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        hp = 100;
+        alive = false;
+        Destroy(gameObject); // Destroy for now
+    }
+}
