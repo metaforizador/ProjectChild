d47c8e9e94667dccd4d0e9b430199e861e11dba9
Toni <toni.heinonen@hotmail.fi>
2020-06-16 12:45:47 +0300

    Load enemy stats from scriptable object

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    public float hp = 100;
-    public bool alive = true;
+    [SerializeField]
+    private EnemySO scriptableObject;
+
+    private float hp, shield, stamina, ammo;
+    private bool alive;
+
+    private float shieldRecovery, staminaRecovery, ammoRecovery, dodgeRate, criticalRate,
+        piercingDmg, kineticDmg, energyDmg, piercingRes, kineticRes, energyRes,
+        attackSpd, movementSpd, fireRate;
+
+    void Start() {
+        hp = 100;
+        shield = 100;
+        stamina = 100;
+        ammo = 100;
+        alive = true;
+
+        shieldRecovery = Stat.CalculateValue(Stat.RECOVERY_MIN_SPEED, Stat.RECOVERY_MAX_SPEED, scriptableObject.shieldRecovery);
+        staminaRecovery = Stat.CalculateValue(Stat.RECOVERY_MIN_SPEED, Stat.RECOVERY_MAX_SPEED, scriptableObject.staminaRecovery);
+        ammoRecovery = Stat.CalculateValue(Stat.RECOVERY_MIN_SPEED, Stat.RECOVERY_MAX_SPEED, scriptableObject.ammoRecovery);
+
+        dodgeRate = Stat.CalculateValue(Stat.DODGE_MIN_PERCENT, Stat.DODGE_MAX_PERCENT, scriptableObject.dodgeRate);
+        criticalRate = Stat.CalculateValue(Stat.CRITICAL_MIN_PERCENT, Stat.CRITICAL_MAX_PERCENT, scriptableObject.criticalRate);
+
+        piercingDmg = Stat.CalculateValue(Stat.DAMAGE_MIN_PERCENT, Stat.DAMAGE_MAX_PERCENT, scriptableObject.piercingDmg);
+        kineticDmg = Stat.CalculateValue(Stat.DAMAGE_MIN_PERCENT, Stat.DAMAGE_MAX_PERCENT, scriptableObject.kineticDmg);
+        energyDmg = Stat.CalculateValue(Stat.DAMAGE_MIN_PERCENT, Stat.DAMAGE_MAX_PERCENT, scriptableObject.energyDmg);
+
+        piercingRes = Stat.CalculateValue(Stat.RESISTANCE_MIN_PERCENT, Stat.RESISTANCE_MAX_PERCENT, scriptableObject.piercingRes);
+        kineticRes = Stat.CalculateValue(Stat.RESISTANCE_MIN_PERCENT, Stat.RESISTANCE_MAX_PERCENT, scriptableObject.kineticRes);
+        energyRes = Stat.CalculateValue(Stat.RESISTANCE_MIN_PERCENT, Stat.RESISTANCE_MAX_PERCENT, scriptableObject.energyRes);
+
+        attackSpd = Stat.CalculateValue(Stat.ATTACK_MIN_SPEED, Stat.ATTACK_MAX_SPEED, scriptableObject.attackSpd);
+        movementSpd = Stat.CalculateValue(Stat.MOVEMENT_MIN_SPEED, Stat.MOVEMENT_MAX_SPEED, scriptableObject.movementSpd);
+        fireRate = Stat.CalculateValue(Stat.FIRE_RATE_MIN_SPEED, Stat.FIRE_RATE_MAX_SPEED, scriptableObject.fireRate);
+    }
 
     public void TakeDamage(float amount) {
         hp -= amount;
@@ -16,7 +50,7 @@ public class Enemy : MonoBehaviour {
     }
 
     private void Die() {
-        hp = 100;
+        hp = 0;
         alive = false;
         Destroy(gameObject); // Destroy for now
     }
