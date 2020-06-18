1861d67f63db8a5e808c4e5397ed8203c70b04d3
Unnis <onni.heinonen98@gmail.com>
2020-06-18 12:10:23 +0300

    strafing added

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -36,31 +36,36 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
+        //player control variables
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
         //checks if player is the air
         isGrounded = Physics.CheckBox(groundCheck.position, new Vector3(5, groundDistance, 5), Quaternion.identity, groundMask);
 
         //sets animator attributes
         animator.SetFloat("Speed", GetComponent<CharacterController>().velocity.magnitude);
 
-        if (Input.GetButton("Fire1")){
-
-            animator.SetBool("Shooting", true);
-        }
-        else
-        {
-            animator.SetBool("Shooting", false);
-        }
-
-        //sets animator attributes
-        animator.SetFloat("Speed", GetComponent<CharacterController>().velocity.magnitude);
+        animator.SetFloat("DirectionX", direction.x);
 
         if (Input.GetButton("Fire1")){
 
             animator.SetBool("Shooting", true);
+
+            if(direction.x != 0 && direction.z == 0)
+            {
+                animator.SetBool("Strafing", true);
+            }
+            else
+            {
+                animator.SetBool("Strafing", false);
+            }
         }
         else
         {
             animator.SetBool("Shooting", false);
+            animator.SetBool("Strafing", false);
         }
 
         if(isGrounded && velocity.y < 0)
@@ -68,15 +73,17 @@ public class playerMovement : MonoBehaviour
             velocity.y = 0;
         }
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //movement
 
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            if (animator.GetBool("Shooting") == false)
+            {
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             xzMovement = moveDir.normalized * speed * Time.deltaTime;
@@ -116,10 +123,13 @@ public class playerMovement : MonoBehaviour
             fireCounter -= Time.deltaTime;
 
             //player turns towards camera while shooting
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(0, 1) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            Debug.Log(direction.x + " " + direction.z);
             //set rotation to angle for smoothing effect
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            
         }
 
         if(fireCounter < 0)
