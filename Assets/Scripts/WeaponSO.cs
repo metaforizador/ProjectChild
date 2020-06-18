a517d0f9cabaafd0d5d197fa858b3112907db1d1
Toni <toni.heinonen@hotmail.fi>
2020-06-18 13:29:56 +0300

    Add Weapon Scriptable Object
    - Add test item Uzi.

---
+++ Assets/Scripts/WeaponSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class WeaponSO : ScriptableObject {
+
+    public new string name;
+    public GameObject weaponPrefab;
+
+    [Header("(Max health and armor is 100)")]
+    public float damagePerBullet;
+    public DamageType weaponType;
+    public float bulletSpeed;
+    [Header("Amount of percentage a bullet consumes ammo")]
+    public int bulletConsumption;
+    [Header("Bullet per second")]
+    public float rateOfFire;
+}
