6f78d4c59d7c925e1d4c0f4e97cbd8ac54d46354
Toni <toni.heinonen@hotmail.fi>
2020-07-08 10:08:31 +0300

    Add ConsumableSO
    - Add enum ConsumableType.
    - Add values for consumable batteries.
    - Create damaged and intact batteries.

---
+++ Assets/Scripts/Editor/ConsumablesSOEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ConsumablesSOEditor)), CanEditMultipleObjects]
+public class ConsumablesSOEditor : Editor {
+
+    public SerializedProperty
+        nameProp, condition, consumableType,
+        // Scanner
+
+        // Battery
+        shieldRecoveryPercentage, boostStaminaRecoverySpeed, boostAmmoRecoverySpeed, boostTimeInSeconds;
+
+    void OnEnable() {
+        // Setup the SerializedProperties
+        nameProp = serializedObject.FindProperty("name");
+        condition = serializedObject.FindProperty("condition");
+        consumableType = serializedObject.FindProperty("consumableType");
+        // Battery
+        shieldRecoveryPercentage = serializedObject.FindProperty("shieldRecoveryPercentage");
+        boostStaminaRecoverySpeed = serializedObject.FindProperty("boostStaminaRecoverySpeed");
+        boostAmmoRecoverySpeed = serializedObject.FindProperty("boostAmmoRecoverySpeed");
+        boostTimeInSeconds = serializedObject.FindProperty("boostTimeInSeconds");
+    }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(nameProp);
+        EditorGUILayout.PropertyField(condition);
+        EditorGUILayout.PropertyField(consumableType);
+
+        ConsumableType type = (ConsumableType)consumableType.enumValueIndex;
+
+        switch (type) {
+            case ConsumableType.Battery:
+                EditorGUILayout.LabelField("Shield recovery percentage", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(shieldRecoveryPercentage);
+                EditorGUILayout.LabelField("Boost by multiplying by this value", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(boostStaminaRecoverySpeed);
+                EditorGUILayout.PropertyField(boostAmmoRecoverySpeed);
+                EditorGUILayout.LabelField("How long boost lasts", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(boostTimeInSeconds);
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/ConsumableSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ConsumableType { Scanner, Battery, ComsatLink, Rig, Scrap, Toy }
+
+[CreateAssetMenu(fileName = "New Consumable", menuName = "Consumable")]
+public class ConsumableSO : PickableSO {
+
+    public ConsumableType consumableType;
+
+    // Battery
+    [Range(0f, 100f)]
+    public float shieldRecoveryPercentage;
+    [Range(1.1f, 3f)]
+    public float boostStaminaRecoverySpeed = 1.1f, boostAmmoRecoverySpeed = 1.1f;
+    [Range(1, 60)]
+    public int boostTimeInSeconds = 1;
+}
