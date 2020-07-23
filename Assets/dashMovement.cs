d797739f8aadbf3a006e3aacdd3a809963e4b059
Unnis <onni.heinonen98@gmail.com>
2020-07-23 12:22:00 +0300

    animation fix

--- Assets/dashMovement.cs
+++ Assets/dashMovement.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class dashMovement : StateMachineBehaviour
 {
     GameObject player;
+    float dashSpeed;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         player = animator.transform.parent.parent.gameObject;
+        dashSpeed = 200;
 
         player.GetComponent<playerMovement>().dashing = true;
     }
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        player.GetComponent<CharacterController>().Move(player.transform.forward * 200 * Time.deltaTime);
+        if (animatorStateInfo.IsName("Armature_Dash"))
+        {
+            player.GetComponent<CharacterController>().Move(player.transform.forward * dashSpeed * Time.deltaTime);
+        }
+
+        if (animatorStateInfo.IsName("Armature_DashEnd"))
+        {
+            dashSpeed -= (500 * Time.deltaTime);
+
+            player.GetComponent<CharacterController>().Move(player.transform.forward * dashSpeed * Time.deltaTime);
+        }
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
