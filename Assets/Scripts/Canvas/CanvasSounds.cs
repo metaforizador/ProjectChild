aa5782e6e43d7b356bdc3e7fe3ee9d785e50f910
Toni <toni.heinonen@hotmail.fi>
2020-07-27 15:04:21 +0300

    Add unfinished main menu scene
    - PlayButtonEnter() to CanvasSounds for playing sounds when cursor enters the button area.
    - Add images full_horizontal and half_horizontal.

--- Assets/Scripts/Canvas/CanvasSounds.cs
+++ Assets/Scripts/Canvas/CanvasSounds.cs
@@ -6,17 +6,23 @@ public class CanvasSounds : MonoBehaviour {
 
     private AudioSource source;
 
+    public AudioClip BUTTON_ENTER;
     public AudioClip BUTTON_SELECT, BUTTON_BACK;
     public AudioClip ITEM_BREAK, ITEM_USAGE_NOTIFICATION;
 
     /// <summary>
-    /// Get audio source component
+    /// Gets audio source component.
     /// </summary>
     void Awake() => source = GetComponent<AudioSource>();
 
     /// <summary>
-    /// Play provided sound once
+    /// Plays provided sound once.
     /// </summary>
     /// <param name="clip">sound to play</param>
     public void PlaySound(AudioClip clip) => source.PlayOneShot(clip);
+
+    /// <summary>
+    /// Plays an audio when cursor hovers on top of a button.
+    /// </summary>
+    public void PlayButtonEnter() => source.PlayOneShot(BUTTON_ENTER);
 }
