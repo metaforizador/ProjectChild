f180a300a7862ae993809d2249769a15df1ba7ec
Toni <toni.heinonen@hotmail.fi>
2020-07-28 14:13:02 +0300

    Scroll menu buttons using up and down buttons
    Clean up ScrollbarIncrementer a lot.

--- Assets/Scripts/Tools/ScrollbarIncrementer.cs
+++ Assets/Scripts/Tools/ScrollbarIncrementer.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 [RequireComponent(typeof(Button))]
-public class ScrollbarIncrementer : MonoBehaviour {
-    public Scrollbar Target;
-    public Button TheOtherButton;
-    public float Step = 0.1f;
+public class ScrollbarIncrementer : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler {
+    public Scrollbar target;
+    private float step = 0.05f; // Amount of steps one click moves the rect
 
-    public void Increment() {
-        if (Target == null || TheOtherButton == null) throw new Exception("Setup ScrollbarIncrementer first!");
-        Target.value = Mathf.Clamp(Target.value + Step, 0, 1);
-        GetComponent<Button>().interactable = Target.value != 1;
-        TheOtherButton.interactable = true;
+    private float holdFrequency = 0.05f; // Seconds to wait when holding
+    public bool increment;
+
+    void Start() {
+        // Enable / disable button based on scrollbar value
+        target.onValueChanged.AddListener((value) => {
+            GetComponent<Button>().interactable = increment ? target.value < 1 : target.value > 0;
+        });
     }
 
-    public void Decrement() {
-        if (Target == null || TheOtherButton == null) throw new Exception("Setup ScrollbarIncrementer first!");
-        Target.value = Mathf.Clamp(Target.value - Step, 0, 1);
-        GetComponent<Button>().interactable = Target.value != 0; ;
-        TheOtherButton.interactable = true;
+    /// <summary>
+    /// Increment and decrement the scroll wheel value.
+    /// </summary>
+    public void MoveScroll() {
+        if (target == null) throw new Exception("Setup ScrollbarIncrementer first!");
+        float value = increment ? target.value + step : target.value - step;
+        target.value = Mathf.Clamp(value, 0, 1);
+    }
+
+    IEnumerator IncrementDecrementSequence(bool increment) {
+        yield return new WaitForSecondsRealtime(holdFrequency);
+        MoveScroll();
+        StartCoroutine("IncrementDecrementSequence", increment);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        MoveScroll();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        StartCoroutine("IncrementDecrementSequence", increment);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        StopCoroutine("IncrementDecrementSequence");
     }
 }
